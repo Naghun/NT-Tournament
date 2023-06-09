fe7012b8a35e149414970f3fd7d06fa673dff153
Naghun <budad-97@hotmail.com>
2023-06-09 16:24:24 +0200

    Draft and cards functionality almost done
    First successful attempts to make drawing 32 random cards with ajax and sessions and some simple js functionality for showing cards in cards.html template

--- game/views.py
+++ game/views.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import Card
@@ -19,18 +20,6 @@ class Cards_view(ListView):
     template_name='game/cards.html'
     context_object_name='pictures_of_cards'
 
-    def get_random_cards(self):
-        random_numbers=random.sample(range(1,81),32)
-        random_cards=Card.objects.filter(id__in=random_numbers)
-        return random_cards
-
-    
-    def get_context_data(self, **kwargs):
-        context= super().get_context_data(**kwargs)
-        context['random_cards']=self.get_random_cards()
-        return context
-
-
 class Bonus_view(TemplateView):
     template_name='game/bonus.html'
 
@@ -39,16 +28,29 @@ class Draft_view(ListView):
     template_name='game/draft.html'
     context_object_name='draft_list'
 
-    def novi_brojevi(self, request):
-        random_numbers = random.sample(range(1, 81), 32)
-        request.session['random_numbers'] = random_numbers
-
-    def post(self, request, *args, **kwargs):
-        if 'novi_brojevi' in request.POST:
-            self.novi_brojevi(request)
-
-        return super().get(request, *args, **kwargs)
+    def get(self, request):
+        cards=self.get_random_cards()
+        self.store_cards_in_sessions(request, cards)
+        context= {'cards': cards}
+        return render(request, 'game/draft.html')
     
+    def post(self, request):
+        cards=self.get_random_cards()
+        self.store_cards_in_sessions(request, cards)
+        context={'cards': cards}
+        return render(request, 'game/draft.html', context)
+
+
+    def get_random_cards(self):
+        all_cards=list(Card.objects.all())
+        random_cards=random.sample(all_cards, 32)
+        return random_cards
+    
+    def store_cards_in_sessions(self, request, cards):
+        card_ids= [card.id for card in cards]
+        request.session['card_ids']=card_ids
+        request.session.modified= True
+
 
 class Tournament_view(TemplateView):
     template_name='game/tournament.html'
---
+++ static/js/cards.js
@@ -0,0 +1,15 @@
+const humans= document.querySelector('.humans');
+const fantasy =document.querySelector('.fantasy');
+
+humans.addEventListener('click', function () {
+    const human_cards=document.querySelectorAll(".humans_cards");
+    human_cards.forEach(element => {
+        element.style.display= 'block'
+    });
+})
+fantasy.addEventListener('click', function () {
+    const fantasy_cards=document.querySelectorAll(".fantasy_cards");
+    fantasy_cards.forEach(element => {
+        element.style.display='block'
+    });
+})
